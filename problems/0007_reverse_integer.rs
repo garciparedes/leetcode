77efbad40d63802af97c241b6ab0aed1d277c340
Sergio Garcia Prado <sergio@garciparedes.me>
2020-03-25 23:29:24 +0100

    * Refactored solution.

--- problems/0007_reverse_integer.rs
+++ problems/0007_reverse_integer.rs
@@ -1,17 +1,18 @@
 struct Solution {}
 
 impl Solution {
+    fn shift(result: i32, current: &i32) -> Option<i32> {
+        Option::Some(result)
+            .and_then(|x| x.checked_mul(10))
+            .and_then(|x| x.checked_add(current % 10))
+    }
+
     pub fn reverse(x: i32) -> i32 {
         let sign = ((x > 0) as i32) * 2 - 1;
-
         let mut current = x.abs();
         let mut result: i32 = 0;
         while current > 0 {
-            result = match result.checked_mul(10) {
-                Some(val) => val,
-                None => return 0,
-            };
-            result = match result.checked_add(&current % 10) {
+            result = match Solution::shift(result, &current) {
                 Some(val) => val,
                 None => return 0,
             };
