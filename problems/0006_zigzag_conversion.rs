6ce1b333dd78e591be2486884dfe48b05a15e024
Sergio García Prado <sergio@garciparedes.me>
2020-05-23 10:57:48 +0200

    * Fixed minor bug related with corner case.

--- problems/0006_zigzag_conversion.rs
+++ problems/0006_zigzag_conversion.rs
@@ -2,12 +2,17 @@ use std::cmp;
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows == 1 || s.len() == 1 {
+            return s;
+        }
+
         let mut rows: Vec<String> = vec![String::new(); cmp::min(num_rows as usize, s.len())];
-        
         let mut row_index = 0;
         let mut direction = false;
         for c in s.chars() {
+            
             rows[row_index].push(c);
+
             if row_index == 0 || row_index == rows.len() - 1 {
                 direction = !direction;
             }
