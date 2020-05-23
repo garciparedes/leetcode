56ca531fa4ffc4929fb7c1be5301df63dbae3b29
Sergio García Prado <sergio@garciparedes.me>
2020-05-23 10:53:28 +0200

    * Done with "ZigZag Conversion" problem.

---
+++ problems/0006_zigzag_conversion.rs
@@ -0,0 +1,19 @@
+use std::cmp;
+
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let mut rows: Vec<String> = vec![String::new(); cmp::min(num_rows as usize, s.len())];
+        
+        let mut row_index = 0;
+        let mut direction = false;
+        for c in s.chars() {
+            rows[row_index].push(c);
+            if row_index == 0 || row_index == rows.len() - 1 {
+                direction = !direction;
+            }
+            row_index = (row_index as i32 + (direction as i32) * 2 - 1) as usize;
+        }
+        
+        return rows.join("");
+    }
+}
