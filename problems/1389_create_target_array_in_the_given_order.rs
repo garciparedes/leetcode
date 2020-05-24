9064476cf524d4548321ce0167dd79cc7ca932d4
Sergio García Prado <sergio@garciparedes.me>
2020-05-24 14:29:48 +0200

    * Improved solution to be more memory-efficient.

--- problems/1389_create_target_array_in_the_given_order.rs
+++ problems/1389_create_target_array_in_the_given_order.rs
@@ -2,13 +2,11 @@ impl Solution {
     pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
         return index
             .iter()
-            .ziºp(nums)
-            .fold(Vec::new(), |vec, (&i, num)| {
-                let mut vec: Vec<i32> = vec.iter().cloned().collect();
+            .zip(nums)
+            .fold(Vec::new(), |mut vec, (&i, num)| {
                 vec.insert(i as usize, num); 
                 return vec;
             })
         ;
     }
 }
-
