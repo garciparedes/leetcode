09cc1138c484a8355d4e86eb085ef3ccda75a234
Sergio García Prado <sergio@garciparedes.me>
2020-06-06 19:20:03 +0200

    * Done with "1179. Reformat Department Table".

---
+++ problems/1179_reformat_department_table.sql
@@ -0,0 +1,23 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    id, 
+    Jan as Jan_Revenue,
+    Feb as Feb_Revenue,
+    Mar as Mar_Revenue,
+    Apr as Apr_Revenue,
+    May as May_Revenue,
+    Jun as Jun_Revenue,
+    Jul as Jul_Revenue,
+    Aug as Aug_Revenue,
+    Sep as Sep_Revenue,
+    Oct as Oct_Revenue,
+    Nov as Nov_Revenue,
+    Dec as Dec_Revenue
+FROM (
+    SELECT id, revenue, month
+    FROM Department
+) AS source
+PIVOT (
+    SUM(revenue)
+    FOR month IN ([Jan],[Feb],[Mar],[Apr],[May],[Jun],[Jul],[Aug],[Sep],[Oct],[Nov],[Dec])
+) AS pivoted;
