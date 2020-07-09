340e9b9a8bbd4a533d08cac6d86acb4c84f5d782
Sergio García Prado <sergiogp@unlimiteck.com>
2020-07-09 09:12:43 +0200

    * Done with "1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix".

--- problems/1284_minimum_number_of_flips_to_convert_binary_matrix_to_zero_matrix.rs
+++ problems/1284_minimum_number_of_flips_to_convert_binary_matrix_to_zero_matrix.rs
@@ -41,14 +41,10 @@ impl Solution {
         if position.1 < m - 1 {
             mat[position.0][position.1 + 1] = 1 - mat[position.0][position.1 + 1];
         }
-        let mut count = i32::max_value();
-
-        if memory.contains_key(&mat) {
-            let another = memory.get(&mat).unwrap();
-            count = cmp::min(count, *another);
+            
+        let mut count = memory.entry(mat.clone()).or_insert_with(i32::max_value).clone();
+        if count != i32::max_value() {
             return count;
-        } else {
-            memory.insert(mat.clone(), count);
         }
 
         for i in 0..n {
@@ -77,4 +73,10 @@ impl Solution {
         }
         return true;
     }
+}
+
+struct Solution {  }
+
+fn main() {
+    println!("{:?}", Solution::min_flips(vec![vec![0,0],vec![0, 1]]));
 }
