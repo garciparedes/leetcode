0985d5633f851b9669b806c094396429b8f18aa5
Sergio García Prado <sergiogp@unlimiteck.com>
2020-08-06 16:36:08 +0200

    * Improved "442. Find All Duplicates in an Array".

--- problems/0442_find_all_duplicates_in_an_array.rs
+++ problems/0442_find_all_duplicates_in_an_array.rs
@@ -1,15 +1,14 @@
-use std::collections::HashSet;
-
 impl Solution {
-    pub fn find_duplicates(nums: Vec<i32>) -> Vec<i32> {
-        let mut memory = HashSet::new();
-        
-        let mut result = HashSet::new();
-        for number in nums {
-            if !memory.insert(number) {
-                result.insert(number);
+    pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut result = Vec::new();
+        for i in 0..nums.len() {
+            let v = nums[i].abs();
+            if nums[v as usize - 1] > 0 {
+                nums[v as usize - 1] *= -1;
+            } else {
+                result.push(v);
             }
         }
-        return result.into_iter().collect();
+        return result;
     }
 }
