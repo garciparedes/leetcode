d1ef44be05c93458279a8439284c90d8b35e5d26
Sergio García Prado <sergio@garciparedes.me>
2020-08-12 11:08:29 +0200

    * Reduced unneeded cloning.

--- problems/1219_path_with_maximum_gold.rs
+++ problems/1219_path_with_maximum_gold.rs
@@ -1,19 +1,19 @@
 use std::cmp;
 
 impl Solution {
-    pub fn get_maximum_gold(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn get_maximum_gold(mut grid: Vec<Vec<i32>>) -> i32 {
         let (n, m) = (grid.len() as i32, grid[0].len() as i32);
         
         let mut best = 0;
         for i in 0..n as i32 {
             for j in 0..m as i32 {
-                best = cmp::max(best, Self::mine(grid.clone(), i, j));
+                best = cmp::max(best, Self::mine(&mut grid, i, j));
             }
         }
         return best;
     }
     
-    fn mine(mut grid: Vec<Vec<i32>>, x: i32, y: i32) -> i32 {
+    fn mine(grid: &mut Vec<Vec<i32>>, x: i32, y: i32) -> i32 {
         let (n, m) = (grid.len() as i32, grid[0].len() as i32);
         if x < 0 || y < 0 || x == n || y == m || grid[x as usize][y as usize] == 0 {
             return 0;
@@ -23,8 +23,9 @@ impl Solution {
         
         let mut best = 0;
         for (i, j) in vec![(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            best = cmp::max(best, Self::mine(grid.clone(), x + i, y + j));
+            best = cmp::max(best, Self::mine(grid, x + i, y + j));
         }
+        grid[x as usize][y as usize] = current;
         best += current;
         return best;
     }
