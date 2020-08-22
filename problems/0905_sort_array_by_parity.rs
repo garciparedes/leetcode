b95d59657f7c3fc82d5c72eff413918c851f63fc
Sergio García Prado <sergiogp@unlimiteck.com>
2020-08-22 14:52:38 +0200

    * Improved "905. Sort Array By Parity".

--- problems/0905_sort_array_by_parity.rs
+++ problems/0905_sort_array_by_parity.rs
@@ -1,18 +1,15 @@
 impl Solution {
-    pub fn sort_array_by_parity(a: Vec<i32>) -> Vec<i32> {
-        let mut array = a.clone();
-        let mut threshold = array.len();
-        let mut i = 0;
-        
-        while i < threshold {
-            if array[i] % 2 == 0 {
+    pub fn sort_array_by_parity(mut a: Vec<i32>) -> Vec<i32> {
+        let (mut i, mut n) = (0, a.len());
+        while i < n {
+            if a[i] % 2 == 0 {
                 i += 1;
+                continue;
             } else {
-                let item = array.remove(i);
-                array.push(item);
-                threshold -= 1;
+                a.swap(i, n - 1);
+                n -= 1;   
             }
-        }
-        return array;
+        }     
+        return a;
     }
 }
