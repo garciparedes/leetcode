f978cf9c42ee9c8b453f8f46d500bb60b001a1c0
Sergio García Prado <sergio@garciparedes.me>
2020-08-27 20:15:44 +0200

    * Improved "436. Find Right Interval".

--- problems/0436_find_right_interval.rs
+++ problems/0436_find_right_interval.rs
@@ -1,21 +1,25 @@
 impl Solution {
     pub fn find_right_interval(intervals: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut aux: Vec<(i32, usize)> = intervals
+            .iter()
+            .cloned()
+            .enumerate()
+            .map(|(i, v)| (v[0], i))
+            .collect();
+        aux.sort_unstable();
         
         let mut result = Vec::new();
         for i in 0..intervals.len() {
             let mut index = -1;
-            for j in 0..intervals.len() {
-                if i == j {
+            for (v, j) in aux.iter() {
+                if i == *j {
                     continue;
                 }
-                if intervals[i][1] > intervals[j][0] {
+                if intervals[i][1] > *v {
                     continue;
                 }
-                if index >= 0 && intervals[j][0] > intervals[index as usize][0] {
-                    continue;
-                }
-                
-                index = j as i32;
+                index = *j as i32;
+                break;
             }
             result.push(index);
         }
