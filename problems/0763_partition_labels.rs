545f4ac1a63be990ce46b399432bf872210fdbf1
Sergio García Prado <sergio@garciparedes.me>
2020-09-04 17:52:03 +0200

    * Improved "763. Partition Labels".

--- problems/0763_partition_labels.rs
+++ problems/0763_partition_labels.rs
@@ -1,38 +1,36 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
+use std::cmp;
 
 impl Solution {
     pub fn partition_labels(s: String) -> Vec<i32> {
-        let mut ranges: HashMap<char, Vec<i32>> = HashMap::new();
-        
-        
-        for (i, c) in s.chars().enumerate() {
-            let mut range = ranges.entry(c).or_insert_with(Vec::new);
-            if range.len() < 2 {
-                range.push(i as i32);    
-            } else {
-                range[1] = i as i32;
-            }   
-        }
-        
-        let mut ranges: Vec<Vec<i32>> = ranges
-            .values()
-            .cloned()
-            .collect();
-        ranges.sort();
-
-        let mut result = Vec::new();
-        let mut low = ranges[0][0];
-        let mut upper = ranges[0][ranges[0].len() - 1];
-        for range in ranges {
-            if range.iter().all(|&v| v > upper)  {
-                result.push(upper - low + 1);
-                low = range[0];
-                upper = range[range.len() - 1];
-            } else if upper < range[range.len() - 1] {
-                upper = range[range.len() - 1];
+        let mut s: Vec<_> = s.chars().collect();
+        let mut processed = HashSet::new();
+        let mut ranges = Vec::new();
+        for i in 0..s.len() {
+            if processed.contains(&s[i]) {
+                continue;
             }
+            processed.insert(s[i]);
+            let mut j = i;
+            for jj in (i..s.len()).rev() {
+                if s[i] == s[jj] {
+                    j = jj;
+                    break;
+                }
+            }
+            ranges.push((i, j));
         }
-        result.push(upper - low + 1);
-        return result;
+        
+        let mut i = 0;
+        while i < ranges.len() - 1 {
+            if ranges[i + 1].0 > ranges[i].1 {
+                i += 1;
+                continue;
+            }
+            ranges[i] = (ranges[i].0, cmp::max(ranges[i].1, ranges[i + 1].1));
+            ranges.remove(i + 1);
+        }
+        
+        return ranges.into_iter().map(|x| (x.1 - x.0 + 1) as i32).collect();
     }
 }
