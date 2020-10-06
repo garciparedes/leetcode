8c55eae3fc8a82303fd87fc94159040ec1ebbef9
Sergio García Prado <sergio@garciparedes.me>
2020-10-06 20:15:58 +0200

    * Improve "701. Insert into a Binary Search Tree".

--- problems/0701_insert_into_a_binary_search_tree.rs
+++ problems/0701_insert_into_a_binary_search_tree.rs
@@ -16,20 +16,20 @@
 //     }
 //   }
 // }
-
 use std::rc::Rc;
 use std::cell::RefCell;
-
 impl Solution {
-    pub fn insert_into_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    pub fn insert_into_bst(
+        root: Option<Rc<RefCell<TreeNode>>>, val: i32
+    ) -> Option<Rc<RefCell<TreeNode>>> {
         match root {
             Some(node) => {
                 if val < node.borrow().val {
-                    let left = Solution::insert_into_bst(node.borrow().left.clone(), val);
-                    node.borrow_mut().left = left;
+                    let tmp = Self::insert_into_bst(node.borrow_mut().left.take(), val);
+                    node.borrow_mut().left = tmp;
                 } else {
-                    let right = Solution::insert_into_bst(node.borrow().right.clone(), val);    
-                    node.borrow_mut().right = right;
+                    let tmp = Self::insert_into_bst(node.borrow_mut().right.take(), val);
+                    node.borrow_mut().right = tmp;
                 }
                 return Some(node);
             },
