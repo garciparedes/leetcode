46dc49e131370f638bcc5034eec7a8eb72bad46b
Sergio García Prado <sergio@garciparedes.me>
2020-10-25 10:50:40 +0100

    * Improve "1510. Stone Game IV".

--- problems/1510_stone_game_iv.rs
+++ problems/1510_stone_game_iv.rs
@@ -1,29 +1,30 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn winner_square_game(n: i32) -> bool {   
+    pub fn winner_square_game(n: i32) -> bool {
         let mut memory = HashMap::new();
-        return Self::rec(n, true, &mut memory);        
+        Self::rec(n, true, &mut memory)
     }
     
     fn rec(n: i32, turn: bool, memory: &mut HashMap<i32, bool>) -> bool {
-        if memory.contains_key(&n) {
-            if memory[&n] {
-                return turn;
-            } else {
-                return !turn;
-            }
+        if n == 0 {
+            return !turn;
         }
-        let mut exp = 1;
-        while i32::pow(exp, 2) <= n {
-            if turn == Self::rec(n - i32::pow(exp, 2), !turn, memory) {
-                memory.insert(n, true);
-                return turn;
-            }
-            exp += 1;
+        if let Some(&v) = memory.get(&n) {
+            return !(v ^ turn);
         }
         
-        memory.insert(n, false);
-        return !turn;
+        let mut ans = !turn;
+        let mut k = (n as f64).sqrt().floor() as i32;
+        while k > 0 {
+            let tmp = Self::rec(n - k.pow(2), !turn, memory);
+            if tmp == turn {
+                ans = tmp;
+                break;
+            }
+            k -= 1;
+        }
+        memory.insert(n, turn == ans);
+        return ans;
     }
 }
