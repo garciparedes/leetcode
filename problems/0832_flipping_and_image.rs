b8110c8f2a0956f8c34733b595dcb01fbb292887
Sergio García Prado <sergio@garciparedes.me>
2020-11-10 19:49:58 +0100

    * Improve "832. Flipping an Image".

--- problems/0832_flipping_and_image.rs
+++ problems/0832_flipping_and_image.rs
@@ -1,14 +1,12 @@
 impl Solution {
     pub fn flip_and_invert_image(a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        return a
-            .iter()
-            .map(|row| 
-                row
-                    .into_iter()
+        a.into_iter()
+            .map(|row| {
+                row.into_iter()
                     .rev()
-                    .map(|&x| if x == 0 { 1 } else { 0 })
-                    .collect::<Vec<i32>>()
-            )
-            .collect();
+                    .map(|cell| -1 * (cell - 1))
+                    .collect()
+            })
+           .collect()
     }
 }
