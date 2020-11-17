68e22788909bb56172f84dc5a244f910675c9361
Sergio García Prado <sergio@garciparedes.me>
2020-11-17 19:18:10 +0100

    * Done with "858. Mirror Reflection".

---
+++ problems/0858_mirror_reflection.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn mirror_reflection(mut p: i32, mut q: i32) -> i32 {
+        while p % 2 == 2 && q % 2 == 0 {
+            p /= 2;
+            q /= 2;
+        }
+        if p % 2 == 0 {
+            return 2;
+        } 
+        if q & 2 == 0 {
+            return 0;
+        }
+        return 1;
+    }
+}
