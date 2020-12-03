e0f6dc8e5cf2e8b1b8473f3a232bd55aae524b34
Sergio García Prado <sergio@garciparedes.me>
2020-12-03 21:05:58 +0100

    * Done with "897. Increasing Order Search Tree".

--- problems/0897_increasing_order_search_tree.rs
+++ problems/0897_increasing_order_search_tree.rs
@@ -19,31 +19,26 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
-    pub fn increasing_bst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        if root.is_none() {
-            return None;
-        }
-        
-        let nodes = Self::extract(root);        
-        for w in nodes.as_slice().windows(2) {
-            if let Some(node) = &w[0] {
-                node.borrow_mut().left = None;
-                node.borrow_mut().right = w[1].clone();
-            }
-        }
-        
-        return nodes[0].clone();
+    pub fn increasing_bst(
+        root: Option<Rc<RefCell<TreeNode>>>
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::helper(root, None)
     }
     
-    fn extract(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
-        match root {
-            Some(node) => {
-                let mut base = Self::extract(node.borrow().left.clone());
-                base.push(Some(node.clone()));
-                base.extend(Self::extract(node.borrow().right.clone()).into_iter());
-                return base;
-            },
-            None => Vec::new(),
+    fn helper(
+        left: Option<Rc<RefCell<TreeNode>>>, 
+        parent: Option<Rc<RefCell<TreeNode>>>
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = left {
+            
+            let right = Self::helper(node.borrow_mut().right.take(), parent);
+            node.borrow_mut().right = right;
+            
+            let left = node.borrow_mut().left.take();
+            
+            return Self::helper(left, Some(node));
+        } else {
+            return parent;
         }
     }
 }
