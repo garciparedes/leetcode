9dca0eee7f3e3c2466aefe78b3c4641ef0eb6e85
Sergio García Prado <sergiogp@unlimiteck.com>
2020-12-27 17:44:44 +0100

    * Done with "1669. Merge In Between Linked Lists".

---
+++ problems/1669_merge_in_between_linked_lists.rs
@@ -0,0 +1,48 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+use std::mem;
+
+impl Solution {
+    pub fn merge_in_between(
+        list1: Option<Box<ListNode>>, 
+        a: i32, 
+        b: i32,
+        mut list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut head = list1.unwrap();
+        
+        let mut current = head.as_mut();
+        for _ in 0..(a - 1) {
+            current = current.next.as_mut().unwrap();
+        }
+        mem::swap(&mut current.next, &mut list2);
+        
+        loop {
+            if current.next.is_none() {
+                break;
+            }
+            current = current.next.as_mut().unwrap();
+        }
+
+        for _ in 0..(b - a + 1) {
+            list2 = list2.unwrap().next;
+        }
+        current.next = list2;
+        
+        return Some(head);
+    }
+}
