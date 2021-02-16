bb1f8018347228919abacf7f7ccfa5bf860bb536
Sergio García Prado <sergio@garciparedes.me>
2021-02-16 20:33:54 +0100

    * Done with "238. Product of Array Except Self".

---
+++ problems/0238_product_of_array_except_self.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        
+        let mut ans = vec![1; n];
+        for i in 1..n {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }        
+        
+        let mut next = 1;
+        for i in (0..n).rev() {
+            ans[i] *= next;
+            next *= nums[i];
+        }
+        
+        return ans;
+    }
+}
