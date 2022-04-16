b6fe380bbb159fe961682989c91f80c22baa2c56
Sergio García Prado <sergio@garciparedes.me>
2022-04-16 20:11:28 +0200

    * Done with "2043. Simple Bank System".

---
+++ problems/2043_simple_bank_system.py
@@ -0,0 +1,33 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if not self.withdraw(account1, money):
+            return False
+        if not self.deposit(account2, money):
+            self.deposit(account1, money)
+            return False
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        self.balance[account - 1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        if self.balance[account - 1] < money:
+            return False
+        self.balance[account - 1] -= money
+        return True
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
