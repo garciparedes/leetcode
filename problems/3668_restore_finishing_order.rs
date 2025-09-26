13954deeef7d0d020cd23a4010216ea9b0cf6213
Sergio García Prado <sergio@garciparedes.me>
2025-09-26 16:18:44 +0200

    feat: optimize "3668. Restore Finishing Order"

--- problems/3668_restore_finishing_order.rs
+++ problems/3668_restore_finishing_order.rs
@@ -1,10 +1,9 @@
 use std::collections::HashSet;
 
 impl Solution {
-    pub fn recover_order(order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
+    pub fn recover_order(mut order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
         let friends_set: HashSet<_> = HashSet::from_iter(friends);
-        order.into_iter()
-            .filter(|friend| friends_set.contains(friend))
-            .collect()
+        order.retain(|friend| friends_set.contains(friend));
+        order
     }
 }
