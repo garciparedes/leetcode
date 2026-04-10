25d3524c9790be9a3b2feeea5710ef34cf32a32e
Sergio García Prado <sergio@garciparedes.me>
2026-04-10 12:12:39 +0200

    chore: improve "1948. Delete Duplicate Folders in System"

--- problems/1948_delete_duplicate_folders_in_system.py
+++ problems/1948_delete_duplicate_folders_in_system.py
@@ -2,56 +2,89 @@ from collections import Counter
 
 class Solution:
     def deleteDuplicateFolder(self, paths: List[List[str]]) -> List[List[str]]:
-        root = self.create_tree(paths)
-        counter = Counter()
-        self.compute_hash(root, counter)
-        filtered = self.filter_hashes(counter)
-        pruned = self.prune_tree(root, filtered)
+        root = Node.from_paths(paths)
+        root.prune_duplicates()
+        return root.to_paths()
         
-        ans = list()
-        self.populate(pruned, ans, [])
-        return ans
+class Node:
 
-    def create_tree(self, paths: List[List[str]]) -> Dict[str, Dict]:
-        root = dict()
+    def __init__(self, folder: Optional[str] = None) -> None:
+        self._folder = folder
+        self._children = dict()
+        self._hash = None
+        self._generic_hash = None
+
+    @staticmethod
+    def from_paths(paths: List[List[str]]) -> Node:
+        root = Node()
         for path in paths:
-            current = root
-            for level in path:
-                current[level] = current.get(level, dict())
-                current = current[level]
+            root._add_path(path)
+        root._compute_hash()
         return root
 
-    def compute_hash(self, root: Dict[str, Dict], counter: Optional[Counter[str]] = None) -> Dict[str, Dict]:
-        hashes = dict()
-        for key, value in root.items():
-            hashed = self.compute_hash(value, counter)
-            v = "".join(sorted(hashed.keys()))
-            if len(value) > 0 and counter is not None:
-                counter[f"(*{v})"] += 1
-            hashes[f"({key}{v})"] = hashed 
-        return hashes
-    
-    def filter_hashes(self, counter: Counter[str]) -> Set[str]:
+    def prune_duplicates(self) -> None:
+        hashes = self._find_duplicates()
+        self._prune(hashes)
+
+    def to_paths(self) -> List[List[str]]: 
+        ans = list()
+        current = list()
+        self._to_paths(ans, current)
+        return ans
+   
+    @staticmethod
+    def _filter_hashes(counter: Counter[str]) -> Set[str]:
         ans = set()
         for key, value in counter.items():
             if value > 1:
                 ans.add(key)
         return ans
 
-    def prune_tree(self, root: Dict[str, Dict], hashes: Set[str]) -> Dict[str, Dict]:
-        ans = dict()
-        for key, value in root.items():
-            pruned = self.prune_tree(value, hashes)
-            hashed = self.compute_hash(value)
-            v = "".join(sorted(hashed.keys()))
-            if f"(*{v})" not in hashes:
-                ans[key] = pruned
-        return ans
-
-    def populate(self, root: Dict[str, Dict], ans: List[List[str]], current: List[str]):
+    def _to_paths(self, ans: List[List[str]], current: List[str]): 
         if len(current) > 0:
             ans.append(current.copy())
-        for key, value in root.items():
+        for key, value in self._children.items():
             current.append(key)
-            self.populate(value, ans, current)
+            value._to_paths(ans, current)
             current.pop()
+
+    def _add_path(self, path: List[str]) -> None:
+        current = self
+        for level in path:
+            current = current._get_child(level)
+
+    def _get_child(self, folder: str) -> None:
+        child = self._children.get(folder, None)
+        if child is None:
+            child = Node(folder)
+            self._children[folder] = child
+        return child
+
+    def _find_duplicates(self) -> Set[str]:
+        counter = Counter()
+        self._count_patterns(counter)
+        return self._filter_hashes(counter)
+
+    def _count_patterns(self, counter: Counter[str]) -> None:
+        for key, value in self._children.items():
+            value._count_patterns(counter)
+            if len(value._children) > 0:
+                counter[value._generic_hash] += 1
+    
+    def _prune(self, hashes: Set[str]) -> None:
+        for key in list(self._children.keys()):
+            value = self._children[key]
+            if value._generic_hash in hashes:
+                self._children.pop(key)
+            else:
+                value._prune(hashes)
+
+    def _compute_hash(self) -> None:
+        for key, value in self._children.items():
+            value._compute_hash()
+        self._hash, self._generic_hash = self._compose_hash()
+
+    def _compose_hash(self) -> Tuple[str, str]:
+        suffix =  "".join(sorted(map(lambda node: node._hash, self._children.values())))
+        return f"{self._folder}{suffix})", f"(*{suffix})"
+
